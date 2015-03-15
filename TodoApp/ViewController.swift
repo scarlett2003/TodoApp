9906d6ed91e1f3e69654ef7c39370a72a3cfeca8
scarlett2003 <jovilee@tpts8.seed.net.tw>
2015-03-15 10:58:55 +0800

    建立內存數據庫

--- TodoApp/ViewController.swift
+++ TodoApp/ViewController.swift
@@ -8,11 +8,23 @@
 
 import UIKit
 
+var todos: [TodoModel] = []
+
+func dateFromString(dateStr: String) -> NSDate? {
+    let dateFormatter = NSDateFormatter()
+    dateFormatter.dateFormat = "yyyy-mm-dd"
+    let date = dateFormatter.dateFromString(dateStr)
+    return date
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //先初始化值
+        todos = [TodoModel(id: "1", image: "child-selected", title: "1.去遊樂場", date: dateFromString("2014-11-2")!)]
     }
 
     override func didReceiveMemoryWarning() {
