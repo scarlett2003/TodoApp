f6b6beb04b63a579c574ffd00823d637122728e2
scarlett2003 <jovilee@tpts8.seed.net.tw>
2015-03-15 14:31:37 +0800

    新增navigationItem

--- TodoApp/ViewController.swift
+++ TodoApp/ViewController.swift
@@ -18,7 +18,7 @@ func dateFromString(dateStr: String) -> NSDate? {
     return date
 }
 
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     
@@ -31,6 +31,9 @@ class ViewController: UIViewController, UITableViewDataSource {
             TodoModel(id: "2", image: "shopping-cart-selected", title: "2. 购物", date: dateFromString("2014-10-28")!),
             TodoModel(id: "3", image: "phone-selected", title: "3. 打电话", date: dateFromString("2014-10-30")!),
             TodoModel(id: "4", image: "travel-selected", title: "4. Travel to Europe", date: dateFromString("2014-10-31")!)]
+        
+        // 新增navigationItem
+        navigationItem.leftBarButtonItem = editButtonItem()
     }
 
     override func didReceiveMemoryWarning() {
@@ -41,16 +44,50 @@ class ViewController: UIViewController, UITableViewDataSource {
     
     // mark - UITableViewDataSource
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 80
+        return todos.count
     }
 
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = self.tableView.dequeueReusableCellWithIdentifier("todoCell") as UITableViewCell
         
+        var todo = todos[indexPath.row] as TodoModel
+        
+        var image = cell.viewWithTag(101) as UIImageView
+        var title = cell.viewWithTag(102) as UILabel
+        var date = cell.viewWithTag(103) as UILabel
+        
+        image.image = UIImage(named: todo.image)
+        title.text = todo.title
+        
+        let locale = NSLocale.currentLocale()
+        let dateFormat = NSDateFormatter.dateFormatFromTemplate("yyyy-mm-dd", options: 0, locale: locale)
+        
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = dateFormat
+        date.text = dateFormatter.stringFromDate(todo.date)
+        
+        
         return cell
     }
+    
+    
+    // mark UITableViewDelegate 使用手勢刪除row資料
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath){
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            todos.removeAtIndex(indexPath.row)
+            self.tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
+        }
+    }
 
-
+    // edit Mode
+    override func setEditing(editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        self.tableView.setEditing(editing, animated: animated)
+    }
+    
+    
+    
+    
 }
 
