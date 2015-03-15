1579846cb0883554d9b9b288ee9b00313d1d8bf7
scarlett2003 <jovilee@tpts8.seed.net.tw>
2015-03-16 00:32:45 +0800

    item的viewController的邏輯

---
+++ TodoApp/DetailViewController.swift
@@ -0,0 +1,80 @@
+//
+//  DetailViewController.swift
+//  TodoApp
+//
+//  Created by vialley-imac on 2015/3/15.
+//  Copyright (c) 2015年 ookiwi. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController,UITextFieldDelegate {
+    
+    
+    @IBOutlet weak var childButton: UIButton!
+    @IBOutlet weak var phoneButton: UIButton!
+    @IBOutlet weak var shoppingButton: UIButton!
+    @IBOutlet weak var travelButton: UIButton!
+    @IBOutlet weak var todoItem: UITextField!
+    @IBOutlet weak var todoDate: UIDatePicker!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        todoItem.delegate = self
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func resetButtons(){
+        childButton.selected = false
+        phoneButton.selected = false
+        shoppingButton.selected = false
+        travelButton.selected = false
+    }
+    
+    @IBAction func childTapped(sender: AnyObject) {
+        resetButtons()
+        childButton.selected = true
+    }
+    @IBAction func phoneTapped(sender: AnyObject) {
+        resetButtons()
+        phoneButton.selected = true
+    }
+    @IBAction func shoppingTapped(sender: AnyObject) {
+        resetButtons()
+        shoppingButton.selected = true
+    }
+    @IBAction func travelTapped(sender: AnyObject) {
+        resetButtons()
+        travelButton.selected = true
+    }
+    
+    @IBAction func okTapped(sender: AnyObject) {
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+        todoItem.resignFirstResponder()
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
